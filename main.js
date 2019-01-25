63fb389895f7716c27ce996295a0d4abe2de2d3e
boiz <hairypriest@hotmail.com>
2019-01-24 18:02:50 -0800

    2nd

--- main.js
+++ main.js
@@ -1,34 +1,49 @@
-const nd=new Date;
-const min=nd.getMinutes();
-const remi=min%5;
+let nd=new Date;
+const oneMin=1000*60;
 
 const trimTime=dateObj=>{
 	const str=dateObj.toLocaleString();
 	const sec=str.substr(15,3);
 	return str.replace(sec,"");
+	//return str;
 }
 
-
-
-const oneMin=1000*60;
 const stamp=dateObj=>{
 	return +new Date(dateObj);
 }
 
+const intv=setInterval(()=>{
+	nd=new Date;
+	my$(".time").value=trimTime(nd);
+},1000);
+
+
+const doIt=dir=>{
+
+	const min=nd.getMinutes();
+	const remi=min%5;
+
+	let newStamp;
+
+	if(dir=="up") newStamp=stamp(nd)+(5-remi)*oneMin;
+	else if(dir=="down") newStamp=stamp(nd)-remi*oneMin;
+	
+	const sd=new Date(newStamp);
+
+	my$(".time").value=trimTime(sd);
+	clearInterval(intv);
+	my$(".time").classList.remove("blink");
+
+}
+
 my$(".time").value=trimTime(nd);
 
-console.log(trimTime(nd));
-
 my$(".up").onclick=()=>{
-	const newStamp=stamp(nd)+(5-remi)*oneMin;
-	const sd=new Date(newStamp);
-	my$(".time").value=trimTime(sd);
+	doIt("up");
 }
 
 my$(".down").onclick=()=>{
-	const newStamp=stamp(nd)-remi*oneMin;
-	const sd=new Date(newStamp);
-	my$(".time").value=trimTime(sd);
+	doIt("down");
 }
 
 //const sd=new Date(nd.toLocalDateString()); //second Date
